606f1561ad1eac1c3b645042b9158cd91b7a005b
Lerish-vZ <lerishavz@gmail.com>
2025-01-21 14:19:54 +0200

    update

--- src/main/java/net/spring/ems_backend/entity/Employee.java
+++ src/main/java/net/spring/ems_backend/entity/Employee.java
@@ -1,4 +1,17 @@
 package net.spring.ems_backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }

eceb0f156389138dbf3de204ea2cfea977d63e99
Lerish-vZ <lerishavz@gmail.com>
2025-01-21 14:24:00 +0200

    update

--- src/main/java/net/spring/ems_backend/entity/Employee.java
+++ src/main/java/net/spring/ems_backend/entity/Employee.java
@@ -1,5 +1,6 @@
 package net.spring.ems_backend.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,19 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity //used to specify a class as a jp entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //to automatically increment id
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
     private String email;
 }

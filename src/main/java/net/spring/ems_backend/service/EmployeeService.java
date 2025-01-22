c20137870118fdb2440b63da2843a421b4259e59
Lerish-vZ <lerishavz@gmail.com>
2025-01-22 14:26:13 +0200

    update

--- src/main/java/net/spring/ems_backend/service/EmployeeService.java
+++ src/main/java/net/spring/ems_backend/service/EmployeeService.java
@@ -1,4 +1,7 @@
 package net.spring.ems_backend.service;
 
+import net.spring.ems_backend.dto.EmployeeDto;
+
 public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
 }

a21a6ebcacd117978c44771e9392abcc0eb9d347
Lerish-vZ <lerishavz@gmail.com>
2025-01-22 14:11:50 +0200

    update

--- src/main/java/net/spring/ems_backend/mapper/EmployeeMapper.java
+++ src/main/java/net/spring/ems_backend/mapper/EmployeeMapper.java
@@ -1,4 +1,17 @@
 package net.spring.ems_backend.mapper;
 
+import net.spring.ems_backend.dto.EmployeeDto;
+import net.spring.ems_backend.entity.Employee;
+
 public class EmployeeMapper {
+    public static EmployeeDto maptoEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    
 }

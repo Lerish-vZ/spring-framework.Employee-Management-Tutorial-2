fe74a46674390b6e619d5432c2001cc610014b28
Lerish-vZ <lerishavz@gmail.com>
2025-01-22 14:35:34 +0200

    update

--- src/main/java/net/spring/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/spring/ems_backend/controller/EmployeeController.java
@@ -1,7 +1,23 @@
 package net.spring.ems_backend.controller;
 
+import lombok.AllArgsConstructor;
+import net.spring.ems_backend.dto.EmployeeDto;
+import net.spring.ems_backend.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@AllArgsConstructor
+@RestController //class can now handle HTTP requests
+@RequestMapping("/api/employees")
 public class EmployeeController {
+    private EmployeeService employeeService;
+
+    //Build Add Employee REST API
+    public ResponseEntity<EmployeeDto> createEmployee(EmployeeDto employeeDto) {
+      EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+      return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
 }

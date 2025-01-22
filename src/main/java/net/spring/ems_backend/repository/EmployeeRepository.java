b6f6d3e737eeae0a2182d8bbdda7744f65869fe5
Lerish-vZ <lerishavz@gmail.com>
2025-01-22 14:07:41 +0200

    update

--- src/main/java/net/spring/ems_backend/repository/EmployeeRepository.java
+++ src/main/java/net/spring/ems_backend/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package net.spring.ems_backend.repository;
 import net.spring.ems_backend.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {//two parameters, first is the type of entity and the second is the type of the primary key
 
 }

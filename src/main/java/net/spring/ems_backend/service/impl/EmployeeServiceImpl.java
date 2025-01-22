902057988e37fd4f3c984d1b95e258bdd4184f70
Lerish-vZ <lerishavz@gmail.com>
2025-01-22 14:28:27 +0200

    update

--- src/main/java/net/spring/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/spring/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,18 @@
 package net.spring.ems_backend.service.impl;
 
-public class EmployeeServiceImpl {
+import lombok.AllArgsConstructor;
+import net.spring.ems_backend.dto.EmployeeDto;
+import net.spring.ems_backend.repository.EmployeeRepository;
+import net.spring.ems_backend.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        return null;
+    }
 }

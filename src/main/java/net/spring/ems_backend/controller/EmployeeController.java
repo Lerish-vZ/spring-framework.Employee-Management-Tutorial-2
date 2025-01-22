d737b74dbeef36813c7e18d24522c0d74586d510
Lerish-vZ <lerishavz@gmail.com>
2025-01-22 14:33:00 +0200

    update

--- src/main/java/net/spring/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/spring/ems_backend/controller/EmployeeController.java
@@ -1,4 +1,7 @@
 package net.spring.ems_backend.controller;
 
-public class EmployeeContoller {
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
 }

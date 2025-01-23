a31c276a628ad33c5a48a950fe7ae7f030b31b6e
Lerish-vZ <lerishavz@gmail.com>
2025-01-23 14:04:44 +0200

    update

--- src/main/java/net/spring/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/spring/ems_backend/controller/EmployeeController.java
@@ -6,11 +6,15 @@ import net.spring.ems_backend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.HandlerMapping;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController //class can now handle HTTP requests
 @RequestMapping("/api/employees")
 public class EmployeeController {
+    private final HandlerMapping resourceHandlerMapping;
     private EmployeeService employeeService;
 
     //Build Add Employee REST API
@@ -25,5 +29,11 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
+    }
+
+    //Build Get All Employee REST API
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+       List<EmployeeDto> employees =  employeeService.getAllEmployees();
+       return ResponseEntity.ok(employees);
     }
 }
